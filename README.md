95742641e1de54e9588844f659f5ee076dfc5813
Marcos Quesada <marcos.quesadas@gmail.com>
2015-12-30 00:56:54 +0100

    Update README.md

---
+++ README.md
@@ -0,0 +1,4 @@
+Arduino Playground
+==================
+
+ The first project: develop an explorer robot, using ultrasounds sensors, digital compass and Stepper motors.
--- basic.ino
+++ basic.ino
@@ -19,7 +19,7 @@ int turnSize = 0;
 int stepSize = 4;
 int back = 1;
 
-// 1/4 reduction One step is 2048
+// 1/4 reduction One lap is 2048
 Stepper motor(512, in1Pin, in2Pin, in3Pin, in4Pin);  
 
 void setup()
@@ -31,9 +31,8 @@ void setup()
 
   pinMode(Trigger, OUTPUT);
   pinMode(Echo, INPUT);
-  // this line is for Leonardo's, it delays the serial interface
-  // until the terminal window is opened
-  while (!Serial);
+/*
+  while (!Serial);*/
 
   Serial.begin(57600);
   motor.setSpeed(20);
@@ -83,19 +82,20 @@ long measureDistance() {
   digitalWrite(Trigger,LOW); /* Stabilize sensor*/
   delayMicroseconds(5);
 
-  digitalWrite(Trigger, HIGH); /* envío del pulso ultrasónico*/
+  digitalWrite(Trigger, HIGH);
   delayMicroseconds(10);
 
-  pulseTime=pulseIn(Echo, HIGH, 30000); /* Función para medir la longitud del pulso entrante. Mide el tiempo que transcurrido entre el envío
-   del pulso ultrasónico y cuando el sensor recibe el rebote, es decir: desde que el pin 12 empieza a recibir el rebote, HIGH, hasta que
-   deja de hacerlo, LOW, la longitud del pulso entrante*/
+/* Función para medir la longitud del pulso entrante. Mide el tiempo que transcurrido entre el envío
+   del pulso ultrasónico y cuando el sensor recibe el rebote, es decir: desde que el pin 12 empieza a
+   recibir el rebote, HIGH, hasta que deja de hacerlo, LOW, la longitud del pulso entrante*/
+  pulseTime=pulseIn(Echo, HIGH, 30000);
 
   if (pulseTime == 0) {
     pinMode(Echo,OUTPUT);
     digitalWrite(Echo,LOW);
     delay(1);
     pinMode(Echo,INPUT);
-    pulseTime = 5882; // means out of range
+    pulseTime = 9999; // means out of range
   }
 
   return microsecondsToCentimeters(pulseTime);
@@ -103,7 +103,7 @@ long measureDistance() {
 
 long microsecondsToCentimeters(long microseconds) {
   // Speed of light is 29.4117647 microseconds by cm
-  //return (microseconds / 29.4117647 /2);
+  //return (microseconds / 29.4117647 /2); // adjust on linear regression
   return (microseconds / 29.4117647 /2.-1.697959)/1.257335;
 } 
 
